4a9740a3ba97785e4deaeb9752fe84e01b112cef
Juan Carlos <97527277+juan-soares@users.noreply.github.com>
2023-05-26 21:15:56 +0000

    Update Objective.tsx

--- src/screens/Objective/Objective.tsx
+++ src/screens/Objective/Objective.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+impor { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import readObjective from "../../services/objective/read";
